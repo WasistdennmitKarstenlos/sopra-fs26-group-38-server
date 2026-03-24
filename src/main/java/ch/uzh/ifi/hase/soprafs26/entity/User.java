c2951d9f8415dc93cbedf06a313d460f2277a2da
lennartrobin.zirn@uzh.ch <lennartrobin.zirn@uzh.ch>
2026-03-24 19:54:56 +0100

    Task30.2 and Task33.2: Backend endpoint and protect authenticated routes

--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/User.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
  * Every variable will be mapped into a database field with the @Column
  * annotation
  * - nullable = false -> this cannot be left empty
- * - unique = true -> this value must be unqiue across the database -> composes
+ * - unique = true -> this value must be unique across the database -> composes
  * the primary key
  */
 @Entity
@@ -32,11 +32,17 @@ public class User implements Serializable {
 	@Column(nullable = false, unique = true)
 	private String username;
 
-	@Column(nullable = false, unique = true)
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = true, unique = true)
 	private String token;
 
 	@Column(nullable = false)
 	private UserStatus status;
+
+	@Column(nullable = false)
+	private String creationDate;
 
 	public Long getId() {
 		return id;
@@ -62,6 +68,14 @@ public class User implements Serializable {
 		this.username = username;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public String getToken() {
 		return token;
 	}
@@ -76,5 +90,13 @@ public class User implements Serializable {
 
 	public void setStatus(UserStatus status) {
 		this.status = status;
+	}
+
+	public String getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(String creationDate) {
+		this.creationDate = creationDate;
 	}
 }
