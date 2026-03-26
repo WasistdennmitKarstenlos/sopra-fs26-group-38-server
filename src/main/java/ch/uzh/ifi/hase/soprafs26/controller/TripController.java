702370e57e8101c71f7184b2bd1b5e4a8d2388fe
3xpr <joel.dickey@protonmail.com>
2026-03-26 15:26:29 +0100

    Task 44: Restrict trip overview endpoint
    close #44

--- src/main/java/ch/uzh/ifi/hase/soprafs26/controller/TripController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/controller/TripController.java
@@ -2,6 +2,7 @@ package ch.uzh.ifi.hase.soprafs26.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import ch.uzh.ifi.hase.soprafs26.entity.Trip;
 import ch.uzh.ifi.hase.soprafs26.rest.dto.TripGetDTO;
 import ch.uzh.ifi.hase.soprafs26.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs26.service.TripService;
+import ch.uzh.ifi.hase.soprafs26.service.UserService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,15 +24,21 @@ import java.util.List;
 public class TripController {
 
 	private final TripService tripService;
+	private final UserService userService;
 
-	TripController(TripService tripService) {
+	TripController(TripService tripService, UserService userService) {
 		this.tripService = tripService;
+		this.userService = userService;
 	}
 
 	@GetMapping("/trips")
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
-	public List<TripGetDTO> getAllTrips() {
+	public List<TripGetDTO> getAllTrips(
+			@RequestHeader(value = "Authorization", required = false) String token) {
+		// Only authenticated users may access trip overview.
+		userService.validateToken(token);
+
 		List<Trip> trips = tripService.getTrips();
 		List<TripGetDTO> tripGetDTOs = new ArrayList<>();
 
