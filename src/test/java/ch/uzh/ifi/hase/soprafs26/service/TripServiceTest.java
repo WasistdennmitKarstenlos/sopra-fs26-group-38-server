fed3f397399f8462b27f90d492d0b16ff830c717
JeanKeim1 <jeanleon.keim@uzh.ch>
2026-04-14 11:19:02 +0200

    Simplify imports in TripServiceTest

--- src/test/java/ch/uzh/ifi/hase/soprafs26/service/TripServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs26/service/TripServiceTest.java
@@ -4,10 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
